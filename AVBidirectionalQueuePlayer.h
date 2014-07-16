8a7418d98de8dd894f0050a229ef4767dc0ebe55
Jacco Taal <jacco@bitnomica.com>
2014-07-16 12:45:01 +0200

    Greatly simplified and cleaned up code, meanwhile fixed number of bugs.                      Renamed to more apt AVBidirectionalQueuePlayer

--- AVBidirectionalQueuePlayer.h
+++ AVBidirectionalQueuePlayer.h
@@ -12,27 +12,25 @@
 //  nowPlayingIndex and NSArray itemsForPlayer. 
 //
 //  Note also that this classrequires that the AVFoundation framework be included in your project.
+//
+//  2014/07/16  (JRTaal) Greatly simplified and cleaned up code, meanwhile fixed number of bugs.
+//                       Renamed to more apt AVBidirectionalQueuePlayer
 
 #import <AVFoundation/AVFoundation.h>
 
-@interface AVQueuePlayerPrevious : AVQueuePlayer
-{
-    // This is a flag used to mark whether an item being added to the queue is being added by playPreviousItem (which requires slightly different functionality then in the general case) or if it is being added by an external call
-    Boolean isCalledFromPlayPreviousItem;
-    
-    // Adding previous song functionality requires two new class variables: one array to hold the items that the player has been initialized with (to re-create the player when necessary), and one NSNumber to keep track of which song is currently playing (to determine from where the player should be re-populated
-    int nowPlayingIndex;
-}
+@interface AVBidirectionalQueuePlayer : AVQueuePlayer
+
 
 @property (nonatomic, strong) NSMutableArray *itemsForPlayer;
 
-// Two methods need to be added to the AVQueuePlayer: one which will play the last song in the queue, and one which will return if the queue is at the beginning (in case the user wishes to implement special behavior when a queue is at its first item, such as restarting a song). A getIndex method to return the current index is also provided.
--(void)playPreviousItem;
--(Boolean)isAtBeginning;
--(int)getIndex;
+// Two methods need to be added to the AVQueuePlayer: one which will play the last item in the queue, and one which will return if the queue is at the beginning (in case the user wishes to implement special behavior when a queue is at its first item, such as restarting a item). A getIndex method to return the current index is also provided.
 
-// This notification is called whenever an item finishes playing; it advances the now playing index by 1.
--(void)songEnded:(NSNotification *)notification;
+-(void)playPreviousItem;
+
+-(Boolean)isAtBeginning;
+
+-(int)currentIndex;
+
 
 
 /* The following methods of AVQueuePlayer are overridden by AVQueuePlayerPrevious:
--- AVBidirectionalQueuePlayer.m
+++ AVBidirectionalQueuePlayer.m
@@ -4,13 +4,27 @@
 //
 //  Created by Daniel Giovannelli on 2/18/13.
 //
+//  2014/07/16  (JRTaal) Greatly simplified and cleaned up code, meanwhile fixed number of bugs.
+//                       Renamed to more apt AVBidirectionalQueuePlayer
 
-#import "AVQueuePlayerPrevious.h"
+#import "AVBidirectionalQueuePlayer.h"
 
-@implementation AVQueuePlayerPrevious
+@implementation AVBidirectionalQueuePlayer {
 
-@synthesize itemsForPlayer = _itemsForPlayer;
+    NSMutableArray * _itemsForPlayer;
+}
 
+-(NSMutableArray *)itemsForPlayer {
+    return _itemsForPlayer;
+}
+
+-(void)setItemsForPlayer:(NSMutableArray *)itemsForPlayer {
+    [self removeAllItems];
+    _itemsForPlayer = itemsForPlayer;
+    for (AVPlayerItem * item in itemsForPlayer) {
+        [super insertItem:item afterItem:nil];
+    }
+}
 // CONSTRUCTORS
 
 -(id)initWithItems:(NSArray *)items
@@ -19,59 +33,41 @@
     self = [super initWithItems:items];
     if (self){
         self.itemsForPlayer = [NSMutableArray arrayWithArray:items];
-        nowPlayingIndex = 0;
-        isCalledFromPlayPreviousItem = NO;
-        for (int songPointer = 0; songPointer < [items count]; songPointer++) {
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(songEnded:)
-                                                     name:AVPlayerItemDidPlayToEndTimeNotification
-                                                   object:[items objectAtIndex:songPointer]];
-        }
     }
     return self;
 }
 
-+ (AVQueuePlayerPrevious *)queuePlayerWithItems:(NSArray *)items
++ (AVBidirectionalQueuePlayer *)queuePlayerWithItems:(NSArray *)items
 {
     // This function just allocates space for, creates, and returns an AVQueuePlayerPrevious from an array.
     // Honestly I think having it is a bit silly, but since its present in AVQueuePlayer it needs to be
     // overridden here to ensure compatability.
-    AVQueuePlayerPrevious *playerToReturn = [[AVQueuePlayerPrevious alloc] initWithItems:items];
+    AVBidirectionalQueuePlayer *playerToReturn = [[AVBidirectionalQueuePlayer alloc] initWithItems:items];
     return playerToReturn;
 }
 
 // NEW METHODS
 
--(void)songEnded:(NSNotification *)notification {
-    // This method is called by NSNotificationCenter when a song finishes playing; all it does is increment
-    // nowPlayingIndex
-    if (nowPlayingIndex < [_itemsForPlayer count] - 1){
-        nowPlayingIndex++;
-    }
-}
 
 -(void)playPreviousItem
 {
     // This function is the meat of this library: it allows for going backwards in an AVQueuePlayer,
-    // basically by clearing the player and repopulating it from the index of the last song played.
-    // It should be noted that if the player is on its first song, this function will do nothing. It will
-    // not restart the song or anything like that; if you want that functionality you can implement it
+    // basically by clearing the player and repopulating it from the index of the last item played.
+    // It should be noted that if the player is on its first item, this function will do nothing. It will
+    // not restart the item or anything like that; if you want that functionality you can implement it
     // yourself fairly easily using the isAtBeginning method to test if the player is at its start.
-    if (nowPlayingIndex>0){
+    int tempNowPlayingIndex = [_itemsForPlayer indexOfObject: self.currentItem];
+
+    if (tempNowPlayingIndex>0){
         [self pause];
-        // Note: it is necessary to have seekToTime called twice in this method, once before and once after re-making the area. If it is not present before, the player will resume from the same spot in the next song when the previous song finishes playing; if it is not present after, the previous song will be played from the same spot that the current song was on.
+        // Note: it is necessary to have seekToTime called twice in this method, once before and once after re-making the area. If it is not present before, the player will resume from the same spot in the next item when the previous item finishes playing; if it is not present after, the previous item will be played from the same spot that the current item was on.
         [self seekToTime:kCMTimeZero toleranceBefore:kCMTimeZero toleranceAfter:kCMTimeZero];
         // The next two lines are necessary since RemoveAllItems resets both the nowPlayingIndex and _itemsForPlayer
-        int tempNowPlayingIndex = nowPlayingIndex;
         NSMutableArray *tempPlaylist = [[NSMutableArray alloc]initWithArray:_itemsForPlayer];
-        [self removeAllItems];
-        isCalledFromPlayPreviousItem = YES;
+        [super removeAllItems];
         for (int i = tempNowPlayingIndex - 1; i < [tempPlaylist count]; i++) {
-            [self insertItem:[tempPlaylist objectAtIndex:i] afterItem:nil];
+            [super insertItem:[tempPlaylist objectAtIndex:i] afterItem:nil];
         }
-        isCalledFromPlayPreviousItem = NO;
-        // The temp index is necessary since removeAllItems resets the nowPlayingIndex
-        nowPlayingIndex = tempNowPlayingIndex - 1;
         // Not a typo; see above comment
         [self seekToTime:kCMTimeZero toleranceBefore:kCMTimeZero toleranceAfter:kCMTimeZero];
         [self play];
@@ -80,20 +76,20 @@
 
 -(Boolean)isAtBeginning
 {
-    // This function simply returns whether or not the AVQueuePlayerPrevious is at the first song. This is
-    // useful for implementing custom behavior if the user tries to play a previous song at the start of
-    // the queue (such as restarting the song).
-    if (nowPlayingIndex == 0){
+    // This function simply returns whether or not the AVQueuePlayerPrevious is at the first item. This is
+    // useful for implementing custom behavior if the user tries to play a previous item at the start of
+    // the queue (such as restarting the item).
+    if ([self currentIndex] == 0){
         return YES;
     } else {
         return NO;
     }
 }
 
--(int)getIndex
+-(int)currentIndex
 {
     // This method simply returns the now playing index
-    return nowPlayingIndex;
+    return [_itemsForPlayer indexOfObject:self.currentItem];
 }
 
 // OVERRIDDEN AVQUEUEPLAYER METHODS
@@ -103,7 +99,6 @@
     // This does the same thing as the normal AVQueuePlayer removeAllItems, but also sets the
     // nowPlayingIndex to 0.
     [super removeAllItems];
-    nowPlayingIndex = 0;
     [_itemsForPlayer removeAllObjects];
 }
 
@@ -115,35 +110,14 @@
     // It also subtracts 1 from the nowPlayingIndex for every time the item shows up in the itemsForPlayer
     // array before the current value.
     [super removeItem:item];
-    int appearancesBeforeCurrent = 0;
-    for (int tracer = 0; tracer < nowPlayingIndex; tracer++){
-        if ([_itemsForPlayer objectAtIndex:tracer] == item) {
-            appearancesBeforeCurrent++;
-        }
-    }
-    nowPlayingIndex -= appearancesBeforeCurrent;
     [_itemsForPlayer removeObject:item];
 }
 
-- (void)advanceToNextItem
-{
-    // The only addition this method makes to AVQueuePlayer is advancing the nowPlayingIndex by 1.
-    [super advanceToNextItem];
-    if (nowPlayingIndex < [_itemsForPlayer count] - 1){
-        nowPlayingIndex++;
-    }
-}
 -(void)insertItem:(AVPlayerItem *)item afterItem:(AVPlayerItem *)afterItem
 {
     // This method calls the superclass to add the new item to the AVQueuePlayer, then adds that item to the
     // proper location in the itemsForPlayer array and increments the nowPlayingIndex if necessary.
     [super insertItem:item afterItem:afterItem];
-    if (!isCalledFromPlayPreviousItem){
-        if ([_itemsForPlayer indexOfObject:item] < nowPlayingIndex)
-        {
-            nowPlayingIndex++;
-        }
-    }
     if ([_itemsForPlayer containsObject:afterItem]){ // AfterItem is non-nil
         if ([_itemsForPlayer indexOfObject:afterItem] < [_itemsForPlayer count] - 1){
             [_itemsForPlayer insertObject:item atIndex:[_itemsForPlayer indexOfObject:afterItem] + 1];
